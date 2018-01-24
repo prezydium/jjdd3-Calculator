3f2a44ae9ea03260d7962dcd7c06fe087a0d5747
Krzysztof Syska <prezydium834@gmail.com>
2018-01-24 16:57:22 +0100

    Multiply with loop

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,11 +6,20 @@ public class Calculator {
     Scanner sc = new Scanner(System.in);
 
     public void multiply(int a, int b){
+        for (int i = 0; i < Math.abs(b); i++) {
+            if (b >=0){
+                c = c + a;
+            }
+            else {
+                c = c - a;
+            }
 
-
-        c=a*b;
-
-        System.out.println(c);
+        }
+        if (a <0 && b<0){
+            System.out.println(Math.abs(c));
+        } else {
+            System.out.println(c);
+        }
     }
 
 }
